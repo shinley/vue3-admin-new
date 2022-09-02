a14598aa853f4f46589c3fe6d83e133373ba1621
shinley <cxl17258@qq.com>
2022-09-02 19:29:50 +0800

    mock 能添加能删除

--- mock/mockserver.js
+++ mock/mockserver.js
@@ -2,7 +2,9 @@ const path = require('path')
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router(path.join(__dirname, 'db.json'))
-const middlewares = jsonServer.defaults({static:path.join(__dirname, 'middleware.js')})
+// const middlewares = jsonServer.defaults({static:path.join(__dirname, 'middleware.js')})
+
+const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 
@@ -12,19 +14,21 @@ server.use(jsonServer.bodyParser)
 server.use(router)
 
 router.render = (req, res) => {
-  if (req.method === 'POST' || req.method === 'PATCH') {
-    res.json({
+  if (req.method === 'POST' || req.method === 'PATCH' || req.method === 'PUT'||req.method === 'POST'||req.method === 'DELETE') {
+    res.jsonp({
       "code": 200,
       "data": "",
       "message": "successs"
+    }) 
+  }else {
+    res.json({
+      "code": 200,
+      "data": res.locals.data,
+      "message": "success"
     })
   }
 }
 
-server.use(jsonServer.rewriter({
-  '/api/*': '/$1',
-  '/blog/:resource/:id/show': '/:resource/:id'
-}))
 
 server.listen(3000, () => {
   console.log('JSON Server is running')
