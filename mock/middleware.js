28c67f76b98c038d325fc145c54ed88a8650f67d
shinley <chenxl4@autoai.com>
2022-09-02 13:41:56 +0800

    修改json-server middleware.js

--- mock/middleware.js
+++ mock/middleware.js
@@ -1,10 +1,10 @@
 module.exports = (req, res, next) => {
+    res.header('X-Hello', 'World')
     if (req.method === 'POST') {
         return res.json({
             "code": 200,
             "message":"success"
           })
     }
-    res.header('X-Hello', 'World')
     next()
   }
