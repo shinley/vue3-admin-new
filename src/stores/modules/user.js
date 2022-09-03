3fba618a0cb14d4aefcdce7294382736511665eb
shinley <cxl17258@qq.com>
2022-09-03 14:02:40 +0800

    完成把token保存到localstorage中

--- src/stores/modules/user.js
+++ src/stores/modules/user.js
@@ -1,19 +1,28 @@
 import login from '@/api/sys'
 import md5 from 'md5'
+import {setItem, getItem} from '@/utils/storage'
+import  { TOKEN } from  '@/constant/index'
 
 export default {
     namespaced: true, 
-    state:  () => {},
-    mutations: {},
+    state:  () => {
+        token: getItem(TOKEN) || ''
+    },
+    mutations: {
+        setToken(state, token) {
+            state.token = token
+            setItem(TOKEN, token)
+        }
+    },
     actions: {
         lg(context, userInfo) {
-            console.log("登录action")
             const  {username, password} = userInfo
             return new Promise((resolve, reject) => {
                 login({
                     username,
                     password: md5(password)
                 }).then(data => {
+                    this.commit('user/setToken', data.data.data.token)
                     resolve()
                 }).catch(err => {
                     reject()
