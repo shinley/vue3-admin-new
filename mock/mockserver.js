fd8c330fc4c8a9b89a1db17acfa6c71deb472021
shinley <cxl17258@qq.com>
2022-09-03 10:28:03 +0800

    修改mock分页查询

--- mock/mockserver.js
+++ mock/mockserver.js
@@ -16,6 +16,18 @@ server.use((req, res, next) => {
   if (req.method === 'POST') { // add your authorization logic here
     req.method = 'PATCH'
   } 
+
+  // 如果是GET请求, 并且有分页参数。 在header中做标记， 并现响应方法中slice
+ if (req.method === 'GET') {
+
+    // 如果请求参数中传了分页参数 page
+    if (req.query.page) {
+      res.header('x-split-page', 'page')
+      res.header('x-page', req.query.page)
+      res.header('x-size', req.query.size)
+    }
+ }
+
   next() // continue to JSON Server router
  })
 
@@ -27,11 +39,42 @@ router.render = (req, res) => {
       "message": "successs"
     }) 
   }else {
-    res.json({
-      "code": 200,
-      "data": res.locals.data,
-      "message": "success"
-    })
+    // 此处判断是否分页
+    const splitPage = res.getHeaders()["x-split-page"]
+
+    const currPage = parseInt(res.getHeaders()["x-page"])
+    const pageSize = parseInt(res.getHeaders()["x-size"])
+    // 如果分页
+    if (splitPage) {
+         dataArray = res.locals.data;
+         let total  = dataArray.length;
+
+        let  totalPage = total % pageSize == 0 ? total/pageSize : total/pageSize + 1 
+
+         let firstIndex = (currPage-1) * pageSize
+         let lastIndex = currPage * pageSize;
+         dataSlice = dataArray.slice(firstIndex, lastIndex)
+        let data = {
+          "currPage": currPage,
+          "totalPage": totalPage,
+          "pageSize": pageSize,
+          "totalSize": total,
+          "list": dataSlice
+        }
+
+      res.json({
+        "code": 200,
+        "data":data,
+        "message": "success"
+      })
+    } else {
+      // 不分页
+      res.json({
+        "code": 200,
+        "data": res.locals.data,
+        "message": "success"
+      })
+    }
   }
 }
 
