0724653125b2260312acdcea78d63c866f50026e
shinley <cxl17258@qq.com>
2022-09-03 11:13:41 +0800

    修改了启动端口， 并增加了代理

--- vite.config.js
+++ vite.config.js
@@ -18,5 +18,16 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    host: '127.0.0.1',
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8888',
+        changeOrigin: true, 
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   }
 })
