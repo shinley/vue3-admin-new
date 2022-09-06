036825c3aa8e81457c44e23d50549ecc2eb3b8a8
shinley <cxl17258@qq.com>
2022-09-06 21:32:45 +0800

    使用js驱动css

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="sidebarmenu">
-    <h1>占位</h1>
+    <div class="logo-container">
+      <el-avatar
+        :size="logoHeight"
+        shape="square"
+        src="https://m.imooc.com/static/wap/static/common/img/logo-small@2x.png"
+      ></el-avatar>
+      <h1 class="logo-title" v-if="sidebarOpened">后台管理系统</h1>
+    </div>
     <el-scrollbar>
       <SidebarMenu />
     </el-scrollbar>
@@ -8,7 +15,30 @@
 </template>
 
 <script setup>
+import { computed } from 'vue'
 import SidebarMenu from './SidebarMenu.vue'
+import { useAppStateStore } from '../../../stores/app'
+const appStore = useAppStateStore()
+let sidebarOpened = computed(() => {
+  return appStore.sidebarOpened
+})
+
+const logoHeight = 44
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.logo-container {
+  height: v-bind(logoHeight) + 'px';
+  padding: 10px 0 22px 0;
+  display: flex;
+  justify-content: center;
+  .logo-title {
+    margin-left: 10px;
+    color: #fff;
+    font-weight: 600;
+    line-height: 50px;
+    font-size: 16px;
+    white-space: nowrap;
+  }
+}
+</style>
